732fb998d3284efe36e3ee460d8b342435b4144a
JB786 <jayant2001bhatia@gmail.com>
2023-08-31 21:47:03 +0530

    install concurrently module to run frontend and backend simultaneously

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>iNoteBook - Your Own Notebook on the Cloud</h1>
     </div>
   );
 }
 
 export default App;
+
+
+// We install "npm i concurrently". This node package helps to run backend and frontend at the same time means we can use "npm start" and "nodemon backend/index.js" simultaneously in one terminal instead of using separate terminals. Also add ["both": "concurrently \"npm start\" \"nodemon backend/index.js\""] in package.json scripts such that we can use command "npm run both" to start both frontend and backend at the same time.
