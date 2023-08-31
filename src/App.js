655d11f48aa4d1fbee2bc4b276514225a137b8a5
JB786 <jayant2001bhatia@gmail.com>
2023-09-01 00:18:19 +0530

    added few components in react, react-router-dom, logo.svg and favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-     <h1>iNoteBook - Your Own Notebook on the Cloud</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+      </Routes>
+
+    </Router>
   );
 }
 
