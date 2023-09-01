00e4d0f6ecd8f792807e29c9c41b667046240bd1
JB786 <jayant2001bhatia@gmail.com>
2023-09-01 22:52:27 +0530

    added useLocation hook from react-router-dom for active class in navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,13 @@
-import React, {useContext,useEffect} from 'react'
-import noteContext from '../context/notes/noteContext'
+import React from 'react'
 
 function About() {
 
-  const a = useContext(noteContext);
-
-  useEffect(()=>{
-    a.update();
-    // eslint-disable-next-line
-  },[])
-
-
   return (
     <div>
-      <h1>About {a.state.name} who's in class {a.state.class}</h1>
+      <h1>About Us</h1>
     </div>
   )
 }
 
 export default About
 
-
-
-// we can also use function which is passed as context in component
